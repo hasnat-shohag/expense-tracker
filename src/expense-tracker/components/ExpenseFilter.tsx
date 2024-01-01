bb54c4c81b2023854ed69dcd7119d0f868278f82
hasnat-shohag <hasnat.ru.ice19@gmail.com>
2024-01-01 12:49:10 +0600

    build expense filter2

--- src/expense-tracker/components/ExpenseFilter.tsx
+++ src/expense-tracker/components/ExpenseFilter.tsx
@@ -1,12 +1,18 @@
-interface Props{
-  onSelectCategory: (category: string) => void;
+interface Props {
+	onSelectCategory: (category: string) => void;
 }
-const ExpenseFilter = ({onSelectCategory}: Props) => {
-	return <select className="p-2" onChange={(event) => onSelectCategory(event.target.value)}>
-    <option value="">All Categories</option>
-    <option value="Food">Food</option>
-    <option value="Office">Office</option>
-  </select>
+
+const ExpenseFilter = ({ onSelectCategory }: Props) => {
+	return (
+		<select
+			className="p-2"
+			onChange={(event) => onSelectCategory(event.target.value)}
+		>
+			<option value="">All Categories</option>
+			<option value="Food">Food</option>
+			<option value="Office">Office</option>
+		</select>
+	);
 };
 
 export default ExpenseFilter;
